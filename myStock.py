0b206c27eadc3ed348a2c3ffaa185173e158bf0f
skyuchoi <skyuchoi@crossent.com>
2024-03-29 19:43:10 +0900

    - 수익률, 잔고 정보 수정

--- myStock.py
+++ myStock.py
@@ -1,12 +1,12 @@
 import datetime as dt
 import FinanceDataReader as fdr
 
-stock_list = [['hynix',65,131677],
-                ['samsung',150,71326],
-                ['apple',110,185],
-                ['amd',32,183],
-                ['google',100,138],
-                ['ms',56,326]]
+stock_list = [['hynix',65,131677,'하이닉스'],
+                ['samsung',150,71326, '삼성전자'],
+                ['apple',110,185, '애플'],
+                ['amd',32,183, 'AMD'],
+                ['google',100,138, '구글'],
+                ['ms',56,326, '마이크로소프트']]
 
 def get_korea_stock_code(name):
     df_list = fdr.StockListing('KRX')
@@ -22,28 +22,33 @@ def get_buget():
     exchange_rate = int(fdr.DataReader('USD/KRW').iloc[-1]['Close'])
 
     df_samsung = fdr.DataReader(get_korea_stock_code("삼성전자"), yesterday, current)
-    samsung_price = int(df_samsung['Close'].values[0])
+    samsung_price = int(df_samsung['Close'].values[-1])
     df_hynix = fdr.DataReader(get_korea_stock_code("SK하이닉스"), yesterday, current)
-    hynix_price = int(df_hynix['Close'].values[0])
+    hynix_price = int(df_hynix['Close'].values[-1])
 
     df_apple = fdr.DataReader('AAPL', yesterday, current)
-    apple_price = int(df_apple['Close'].values[0])
+    apple_price = int(df_apple['Close'].values[-1])
     df_ms = fdr.DataReader('MSFT', yesterday, current)
-    ms_price = int(df_ms['Close'].values[0])
+    ms_price = int(df_ms['Close'].values[-1])
     df_google = fdr.DataReader('GOOGL', yesterday, current)
-    google_price = int(df_google['Close'].values[0])
+    google_price = int(df_google['Close'].values[-1])
     df_amd = fdr.DataReader('AMD', yesterday, current)
-    amd_price = int(df_amd['Close'].values[0])
+    amd_price = int(df_amd['Close'].values[-1])
 
     price_list = {'hynix':hynix_price, 'samsung':samsung_price, 'apple':apple_price,
                   'amd':amd_price, 'ms':ms_price, 'google':google_price}
 
-    # print(hynix_price)
-    # print(samsung_price)
-    # print(apple_price)
-    # print(amd_price)
-    # print(ms_price)
-    # print(google_price)
+    print("-----------------------------------------------------------------\n")
+    for data in stock_list:
+        if data[0] != 'samsung' and data[0] != 'hynix':
+            print(data[3] + ": ")
+            print("(비중: " + "{0:,}원".format(data[1]*data[2]*exchange_rate) + ")")
+            print("{0:>30,}달러".format(price_list[data[0]]) + " (매수가격" + "{0:,}달러".format(data[2]) + ", 수익률: " + str(round((price_list[data[0]]- data[2])/data[2]*100,2)) + "%)")
+        else:
+            print(data[3] + ": ")
+            print("(비중: " + "{0:,}원".format(data[1]*data[2]) + ")")
+            print("{0:>30,}원".format(price_list[data[0]]) + " (매수가격" + "{0:,}원".format(data[2]) + ", 수익률: " + str(round((price_list[data[0]]- data[2])/data[2]*100,2)) + "%)")
+
     for i in stock_list:
         rate = 1
         if i[0] != 'samsung' and i[0] != 'hynix':
@@ -54,27 +59,14 @@ def get_buget():
 
 if __name__ == '__main__':
     seed, total = get_buget()
-    print("\n---------------------------------------------")
-    print("------- 상규 & 인영 주식 투자 내역 ----------")
-    # print("---------------------------------------------")
-    # print("하이닉스 : ")
-    # print("{0:>30,} 원".format(total))
-    # print("삼성: ")
-    # print("{0:>30,} 원".format(total))
-    # print("애플: ")
-    # print("{0:>30,} 원".format(total))
-    # print("AMD: ")
-    # print("{0:>30,} 원".format(total))
-    # print("MS: ")
-    # print("{0:>30,} 원".format(total))
-    # print("Google: ")
-    # print("{0:>30,} 원".format(total))
-    print("---------------------------------------------")
+    print("\n-----------------------------------------------------------------")
+    print("-------------------- 상규 & 인영 주식 투자 내역 -----------------------")
+    print("-----------------------------------------------------------------\n")
     print("시드 : ")
     print("{0:>30,} 원".format(seed))
     print("평가액 : ")
     print("{0:>30,} 원".format(total))
     print("수익율 : ")
-    print("{0:>30,} %".format(round((total - seed)/seed*100)))
-    print("---------------------------------------------\n")
+    print("{0:>30,} 원".format(total - seed) + " ({0:>3,} %".format(round((total - seed)/seed*100, 2)) + ")")
+    print("\n-----------------------------------------------------------------\n")
     
